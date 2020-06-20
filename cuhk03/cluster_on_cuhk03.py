16dfeeeaf3c4256788bbad1ac5cdaee9a8dfd0b4
ThinkerPcw <cw.peng@foxmail.com>
2020-06-20 16:43:41 +0800

    Default Changelist

---
+++ cuhk03/cluster_on_cuhk03.py
@@ -0,0 +1,18 @@
+import os
+import torchreid
+
+datamanager = torchreid.data.ImageDataManager(
+    root='./',
+    sources='cuhk03',
+    targets='cuhk03',
+    height=256,
+    width=128,
+    batch_size_train=32,
+    batch_size_test=100,
+    transforms=['random_flip', 'random_crop']
+)
+
+data_loader = datamanager.train_loader
+dataset = data_loader.dataset.train + data_loader.dataset.query + data_loader.dataset.gallery
+
+print(len(dataset))
