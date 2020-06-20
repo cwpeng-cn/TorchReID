b2309b20d5d8245f659c48d1d9e738a22e44e715
ThinkerPcw <cw.peng@foxmail.com>
2020-06-20 18:41:06 +0800

    Default Changelist

--- cuhk03/cluster_on_cuhk03.py
+++ cuhk03/cluster_on_cuhk03.py
@@ -40,18 +40,6 @@ def get_features():
     mat_name = os.path.join("./", 'feature_result.mat')
     online_mat_name = "/content/drive/My Drive/Colab/ReID works/CVPR fintuning/mat/market_feature.mat"
 
-    if os.path.exists(mat_name):
-        result = scipy.io.loadmat(mat_name)
-        train_feature = result['train_feature']
-        return train_feature
-
-    if os.path.exists(online_mat_name):
-        with open(online_mat_name, "rb") as f, open(mat_name, 'wb') as fw:
-            fw.write(f.read())
-        result = scipy.io.loadmat(mat_name)
-        train_feature = result['train_feature']
-        return train_feature
-
     datamanager = torchreid.data.ImageDataManager(
         root='./',
         sources='cuhk03',
@@ -66,9 +54,22 @@ def get_features():
     info = datamanager.train_loader
     data_info = info.dataset.train + info.dataset.query + info.dataset.gallery
 
-    net = model.get_model()
     data_loader = get_loader(data_info)
+    train_id, train_camera, train_path = data_loader.dataset.original_id, data_loader.dataset.cameras, data_loader.dataset.paths
 
+    if os.path.exists(mat_name):
+        result = scipy.io.loadmat(mat_name)
+        train_feature = result['train_feature']
+        return train_feature, train_id, train_camera, train_path
+
+    if os.path.exists(online_mat_name):
+        with open(online_mat_name, "rb") as f, open(mat_name, 'wb') as fw:
+            fw.write(f.read())
+        result = scipy.io.loadmat(mat_name)
+        train_feature = result['train_feature']
+        return train_feature, train_id, train_camera, train_path
+
+    net = model.get_model()
     train_feature = FO.extract_cnn_feature(net, loader=data_loader, vis=False, is_normlize=False)
     train_id, train_camera, train_path = data_loader.dataset.original_id, data_loader.dataset.cameras, data_loader.dataset.paths
 
