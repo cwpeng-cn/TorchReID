117c15c05cfca3fbb1fc691289eafb16a50f1335
ThinkerPcw <cw.peng@foxmail.com>
2020-06-20 17:07:40 +0800

    Default Changelist

--- cuhk03/model.py
+++ cuhk03/model.py
@@ -7,15 +7,15 @@ import zipfile
 
 
 def download_and_prepare():
-    reid_path = "/content/My Drive/Colab/datasets/reid.zip"
+    reid_path = "/content/drive/My Drive/Colab/datasets/reid.zip"
     file_zip = zipfile.ZipFile(reid_path, 'r')
     for file in file_zip.namelist():
         file_zip.extract(file, r'.')
 
-    with open("../pcw/My Drive/Colab/ReID works/CVPR fintuning/resnet_ibn_b.py", "rb") as f, open('./resnet_ibn_b.py',
+    with open("/content/drive/My Drive/Colab/ReID works/CVPR fintuning/resnet_ibn_b.py", "rb") as f, open('./resnet_ibn_b.py',
                                                                                                   'wb') as fw:
         fw.write(f.read())
-    with open("../pcw/My Drive/Colab/ReID works/CVPR fintuning/net_149.pth", "rb") as f, open('./storage/net_149.pth',
+    with open("/content/drive/My Drive/Colab/ReID works/CVPR fintuning/net_149.pth", "rb") as f, open('./net_149.pth',
                                                                                               'wb') as fw:
         fw.write(f.read())
 
